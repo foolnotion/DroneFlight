b498b4ae3e7aa6083e027eaf8fe458becff48a32
Gabriel Marchidan <gabriel.marchidan@feel-it-services.com>
2015-12-10 13:51:24 +0200

    initial commit

---
+++ DroneFlightPath/CodeInterpreter/Interpreter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeInterpreter
+{
+    public class Interpreter
+    {
+        public Interpreter()
+        {
+
+        }
+        public List<string> Read()
+        {
+            var moves = new List<string>();
+
+            return moves;
+        }
+    }
+}
---
+++ DroneFlightPath/DroneFlightPath/Program.cs
@@ -0,0 +1,19 @@
+﻿using CodeInterpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneFlightPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interpreter = new Interpreter();
+
+            interpreter.Read();
+        }
+    }
+}
