78abff51983b64fdaed788f52d073f680cbb64ae
Bogdan Burlacu <evil_konkrane@hotmail.com>
2015-12-15 11:02:54 +0100

    Clean up code a little and add separate strategy classs

--- DroneFlightPath/DroneFlightPath/Program.cs
+++ DroneFlightPath/DroneFlightPath/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using CodeInterpreter;
@@ -15,83 +14,21 @@ namespace DroneFlightPath {
       {4, "LEFT"}
     };
 
-
     static void Main(string[] args) {
-      Func<int, AstNode> _ = AstNode.Constant;
-      Func<int, AstNode> mem = AstNode.Pointer;
-
       var sb = new StringBuilder();
-
-      var arr = (VariableAstNode)AstNode.Variable("arr", 10);
-
-      var right = _(2);
-      var left = _(4);
-      var down = _(3);
-      var up = _(1);
-      var hold = _(0);
-
-      var cx = AstNode.Variable("cx");
-      var cy = AstNode.Variable("cy");
-      var tx = AstNode.Variable("tx");
-      var ty = AstNode.Variable("ty");
-      var drone = mem(0);
-      var ret = new AstReturnNode();
-      var block = AstNode.Block(
-        AstNode.Assign(cx, mem(3)),
-        AstNode.Assign(cy, mem(4)),
-        AstNode.Assign(tx, mem(5)),
-        AstNode.Assign(ty, mem(6)),
-        AstNode.IfThen(
-          cy < ty,
-          AstNode.Block(AstNode.Assign(drone, down), ret)
-        ),
-        AstNode.IfThen(
-          cy > ty,
-          AstNode.Block(AstNode.Assign(drone, up), ret)
-        ),
-        AstNode.IfThen(
-          cx > tx,
-          AstNode.Block(AstNode.Assign(drone, left), ret)
-        ),
-        AstNode.IfThen(
-          cx < tx,
-          AstNode.Block(AstNode.Assign(drone, right), ret)
-        ),
-        ret
-      );
-
+      var block = Strategy.BasicStrategy();
       var mmapVisitor = new MapObjectsToMemoryVisitor();
       block.Accept(mmapVisitor);
       var genVisitor = new GenerateAsmVisitor(mmapVisitor.MemoryMap);
       block.Accept(genVisitor);
-      //      genVisitor.Code.Add(Instruction.Hlt());
-
       sb.Clear();
       for (int i = 0; i < genVisitor.Code.Count; ++i) {
         sb.AppendLine($"{genVisitor.Code[i]}");
       }
-      File.WriteAllText(@"C:\\Users\\Bogdan\\Projects\\TechOn2015\\DroneFlightPath\\DroneFlightPath\\TestFile\\07_intoTheDark.txt", sb.ToString());
-      //      Console.WriteLine("Generated ASM:");
-      //      Console.WriteLine(sb);
-      //
-      //      foreach (var p in genVisitor.NodeNames) {
-      //        Console.WriteLine($"{genVisitor.JumpLocations[p.Key]} {p.Value}");
-      //      }
-
+      File.WriteAllText(@"C:\\Users\\P40913\\Projects\\TechOn2015\\DroneFlightPath\\DroneFlightPath\\TestFile\\07_intoTheDark.txt", sb.ToString());
       var rm = new RegisterMachine();
       rm.LoadIntructions(genVisitor.Code);
       rm.Run();
-      //
-      //      var addr = genVisitor.MemoryMap["arr"];
-      //      Console.WriteLine("Arr addr: " + addr);
-      //      Console.WriteLine("A addr: " + genVisitor.MemoryMap["a"]);
-      //      for (int i = addr; i < addr + arr.Size; ++i)
-      //        Console.WriteLine($"{arr.VariableName}[{i - addr}]: {rm.Memory[i]}");
-      //
-      //      var resultAddr = genVisitor.MemoryMap["result"];
-      //      Console.WriteLine($"Result addr: {resultAddr}");
-      //      Console.WriteLine($"Result: {rm.Memory[resultAddr]}");
-      //      Console.Read();
     }
   }
 }
---
+++ DroneFlightPath/DroneFlightPath/Strategy.cs
@@ -0,0 +1,56 @@
+﻿using ast = CodeInterpreter.AST.AstNode;
+
+
+namespace DroneFlightPath {
+  public static class Strategy {
+    public static ast _(int v) { return ast.Constant(v); }
+    public static ast _(string v, int size = 1) { return ast.Variable(v, size); }
+    public static ast Mem(int v) { return ast.Pointer(v); }
+    public static ast Set(ast left, ast right) { return ast.Assign(left, right); }
+    public static ast If(ast condition, ast trueBranch, ast falseBranch = null) {
+      return falseBranch == null ? ast.IfThen(condition, trueBranch) : ast.IfThenElse(condition, trueBranch, falseBranch);
+    }
+    public static ast Block(params ast[] nodes) { return ast.Block(nodes); }
+
+    private static readonly ast hold = _(0);
+    private static readonly ast up = _(1);
+    private static readonly ast right = _(2);
+    private static readonly ast down = _(3);
+    private static readonly ast left = _(4);
+    private static readonly ast drone = Mem(0);
+
+    public static ast BasicStrategy() {
+      // define variables
+      var cx = _("cx");
+      var cy = _("cy");
+      var tx = _("tx");
+      var ty = _("ty");
+      var ret = ast.Return();
+      var block = ast.Block(
+        Set(cx, Mem(3)),
+        Set(cy, Mem(4)),
+        Set(tx, Mem(5)),
+        Set(ty, Mem(6)),
+        If(
+          cy < ty,
+          Block(Set(drone, down), ret)
+        ),
+        If(
+          cy > ty,
+          Block(Set(drone, up), ret)
+        ),
+        If(
+          cx > tx,
+          Block(Set(drone, left), ret)
+        ),
+        If(
+          cx < tx,
+          Block(Set(drone, right), ret)
+        ),
+        ret
+      );
+
+      return block;
+    }
+  }
+}
